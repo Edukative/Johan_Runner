64ce708eb97f196de86c31c35bf00606a5c7ddd9
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-24 19:32:29 +0200

    Started New project (runner)

---
+++ Johan Sené Runner/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
---
+++ Johan Sené Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+
+
+    public float JumpForce;
+
+    public float GravityModifier;
+
+
+    bool IsOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+
+        Physics.gravity *= GravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            playerRB.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+            IsOnGround = true;
+    }
+        
+    
+}
+

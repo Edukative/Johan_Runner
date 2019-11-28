00a69defe85b32110c9664164809dc817359deed
Edukative <56083723+Edukative@users.noreply.github.com>
2019-11-28 19:36:10 +0100

    Lives
    We've add lives

--- Johan Sené Runner/Assets/Scripts/PlayerController.cs
+++ Johan Sené Runner/Assets/Scripts/PlayerController.cs
@@ -26,6 +26,13 @@ public class PlayerController : MonoBehaviour
 
     public AudioClip explosionAudio;
     public AudioClip jumpAudio;
+
+    public int hp;
+
+    private SpriteRenderer hp1;
+    private SpriteRenderer hp2;
+    private SpriteRenderer hp3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,8 +43,13 @@ public class PlayerController : MonoBehaviour
         playerAnim = GetComponent<Animator>();
 
         playerAudio = GetComponent<AudioSource>();
-    }
 
+        GameObject canvas = GameObject.Find("Canvas");
+        hp1 = canvas.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        hp2 = canvas.transform.GetChild(1).GetComponent<SpriteRenderer>();
+        hp3 = canvas.transform.GetChild(2).GetComponent<SpriteRenderer>();
+    }
+ 
     // Update is called once per frame
     void Update()
     {
@@ -50,9 +62,38 @@ public class PlayerController : MonoBehaviour
             playerAudio.PlayOneShot(jumpAudio);
             
         }
-    }
+    }   
+    void loseHp()
+    {
+        if (hp >= 0)
+        {
+            hp--;
+            switch (hp)
+            {
+                case 2: hp3.gameObject.SetActive(false);
+                    break;
+                case 1: hp2.gameObject.SetActive(false);
+                    break;
+                case 0: hp1.gameObject.SetActive(false);
+                    isGameOver = true;
 
-    private void OnCollisionEnter (Collision collision)
+                    playerAnim.SetBool("Death_b", true);
+                    playerAnim.SetInteger("DeathType_int", 1);
+
+                    explosion.Play();
+                    dirt.Stop();
+                    playerAudio.PlayOneShot(explosionAudio);
+
+                    break;
+                default: hp3.gameObject.SetActive(true);
+                         hp2.gameObject.SetActive(true);
+                         hp1.gameObject.SetActive(true);
+                    break;
+
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
@@ -61,15 +102,9 @@ public class PlayerController : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            isGameOver = true;
-            explosion.Play();
-            Debug.Log("F");
-            playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1);
-            playerAudio.PlayOneShot(explosionAudio);
+            loseHp();
+            Destroy(collision.gameObject);
         }
-    }
-        
-    
+    }     
 }
 
